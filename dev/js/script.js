659dc8f62ae2d7e34cefdebe1ce1d7ba92a1160f
Madushan Dasanayaka <madushan.dasanayaka@salmat.com.au>
2017-03-31 16:14:09 +0530

    no message

--- dev/js/script.js
+++ dev/js/script.js
@@ -11,6 +11,43 @@ jQuery.noConflict();
 
 		$('.parallax-window').parallax();
 
-	});	
+		$('.slick').slick({
+			  dots: false,
+			  infinite: false,
+			  speed: 300,
+			  slidesToShow: 5,
+			  slidesToScroll: 1,
+			  responsive: [
+			    {
+			      breakpoint: 992,
+			      settings: {
+			        slidesToShow: 3,
+			        slidesToScroll: 1
+			      }
+			    },
+			    {
+			      breakpoint: 600,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1
+			      }
+			    }
+			  ]
+			});
+		});
+
+		$('.rating').each(function(){
+			var val = $(this).data('value');
+
+			if(val && val>=0 && val<=5){
+				$(this).rateYo({
+					rating: val,
+					starWidth: '12px',
+					ratedFill: '#518c3c',
+					normalFill: "#dddddd",
+					readOnly: true
+				});
+			}
+		});
 
 }(jQuery));

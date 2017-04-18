becd06a1607a91786540f081950c0227132a9f55
Madushan Dasanayaka <madushan.dasanayaka@salmat.com.au>
2017-04-18 17:14:37 +0530

    no message

--- dev/js/script.js
+++ dev/js/script.js
@@ -94,6 +94,31 @@ jQuery.noConflict();
 			    blueimp.Gallery(links, options);
 			});
 
+			if(!isMobileDevice()){
+				$("#stickyNav").sticky({topSpacing:0});
+				$("#stickyForm").sticky({topSpacing:0});
+
+				$('#stickyNav').onePageNav({
+					currentClass: 'active',
+					changeHash: false,
+					scrollSpeed: 750,
+					scrollThreshold: 0.5,
+					filter: '',
+					easing: 'swing',
+					begin: function() {
+						//I get fired when the animation is starting
+					},
+					end: function() {
+						//I get fired when the animation is ending
+					},
+					scrollChange: function($currentListItem) {
+						//I get fired when you enter a section and I pass the list item of the section
+					}
+				});
+				
+			}
+						
+
 		});		
 
 }(jQuery));
